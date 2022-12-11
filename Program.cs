010ab7900cfed4979e264eb59770c6f31dae4c67
Hunor József Nagy <Nhunor06@gmail.com>
2022-12-11 15:12:07 +0100

    Release 1.0

--- Program.cs
+++ Program.cs
@@ -11,18 +11,15 @@ namespace Hash_Calculator_c_Sharp
             while (true)
             {
                 Console.Clear();
-                // Ask the user for the file path
                 Console.Write("Enter the file path: ");
                 string filePath = Console.ReadLine();
                 string userHash;
 
-                // Check if the file path starts and ends with " and remove them if they exist
                 if (filePath.StartsWith('"') && filePath.EndsWith('"'))
                 {
                     filePath = filePath.Substring(1, filePath.Length - 2);
                 }
 
-                // Calculate the MD5 hash of the file
                 using (var md5 = MD5.Create())
                 {
                     using (var stream = File.OpenRead(filePath))
@@ -30,11 +27,9 @@ namespace Hash_Calculator_c_Sharp
                         byte[] hash = md5.ComputeHash(stream);
                         string md5Hash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
 
-                        // Ask the user for the MD5 hash
                         Console.Write("Enter the MD5 hash: ");
                         userHash = Console.ReadLine();
 
-                        // Compare the two hashes
                         if (md5Hash == userHash)
                         {
                             Console.WriteLine("The hashes match.");
@@ -48,7 +43,6 @@ namespace Hash_Calculator_c_Sharp
                     }
                 }
 
-                // Wait for the user to type q to exit the program, or r to clear the console and restart the program
                 Console.WriteLine("Press r to restart, q to quit the program.");
 
                 var key = Console.ReadKey().KeyChar;
