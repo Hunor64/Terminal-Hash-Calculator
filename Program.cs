e9abe75e074a763b37ada6dcbcbbb1b86b97aa10
Hunor József Nagy <Nhunor06@gmail.com>
2022-12-12 16:07:01 +0100

    Release 1.2

--- Program.cs
+++ Program.cs
@@ -2,18 +2,22 @@ using System;
 using System.IO;
 using System.Security.Cryptography;
 
-namespace Terminal-Hash-Calculator
+namespace TerminalHashCalculator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string md5Hash = "";
+            bool runSucsess = false;
+            bool hashMatch = false;
+            string userHash = "";
             while (true)
             {
+                runSucsess = false;
                 Console.Clear();
                 Console.Write("Enter the file path: ");
                 string filePath = Console.ReadLine();
-                string userHash;
 
                 if (filePath.StartsWith('"') && filePath.EndsWith('"'))
                 {
@@ -22,29 +26,26 @@ namespace Terminal-Hash-Calculator
 
                 if (File.Exists(filePath))
                 {
+                    runSucsess = true;
+                    var md5 = MD5.Create();
+                    var stream = File.OpenRead(filePath);
+                    byte[] hash = md5.ComputeHash(stream);
+                    md5Hash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
 
+                    Console.Write("Enter the MD5 hash: ");
+                    userHash = Console.ReadLine();
 
-                    using (var md5 = MD5.Create())
+                    if (md5Hash == userHash)
                     {
-                        using (var stream = File.OpenRead(filePath))
-                        {
-                            byte[] hash = md5.ComputeHash(stream);
-                            string md5Hash = BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
-
-                            Console.Write("Enter the MD5 hash: ");
-                            userHash = Console.ReadLine();
-
-                            if (md5Hash == userHash)
-                            {
-                                Console.WriteLine("The hashes match.");
-                            }
-                            else
-                            {
-                                Console.WriteLine("The hashes do not match.");
-                                Console.WriteLine($"Calculated hash: {md5Hash}");
-                                Console.WriteLine($"User input hash: {userHash}");
-                            }
-                        }
+                        Console.WriteLine("The hashes match.");
+                        hashMatch = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("The hashes do not match.");
+                        Console.WriteLine($"Calculated hash: {md5Hash}");
+                        Console.WriteLine($"User input hash: {userHash}");
+                        hashMatch = false;
                     }
                 }
                 else
@@ -52,7 +53,7 @@ namespace Terminal-Hash-Calculator
                     Console.WriteLine($"Directory {filePath} does not exist!");
                 }
 
-                Console.WriteLine("Press r to restart, q to quit the program.");
+                Console.WriteLine("Press r to restart, q to quit the program or s to save it.");
 
                 var key = Console.ReadKey().KeyChar;
 
@@ -60,14 +61,53 @@ namespace Terminal-Hash-Calculator
                 {
                     break;
                 }
-                if (key == 's')
+                else if (key == 's' && runSucsess == true)
                 {
-                    
+                    Console.WriteLine();
+                    Console.Write("Enter save location: ");
+                    var saveLocation = Console.ReadLine();
+                    if (hashMatch)
+                    {
+                        File.WriteAllText(saveLocation, "The hashes matched!" + "\n" + "The calculated hash was: " + md5Hash);
+                    }
+                    else if (!hashMatch)
+                    {
+                        File.WriteAllText(saveLocation, "The hashes did not match" + "\n" + "The calculated hash was: " + md5Hash + "\n" + "User input hash was: " + userHash);
+                    }
+
+                    Console.WriteLine("Saved!");
+                    Console.WriteLine("Press r to restart, q to quit the program.");
+                    key = Console.ReadKey().KeyChar;
+                    if (key == 'q')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                }
+                else if (key == 's' && runSucsess == false)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("No hash was calculated!");
+                    Console.WriteLine("Press r to restart, q to quit the program.");
+                    key = Console.ReadKey().KeyChar;
+                    if (key == 'q')
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        continue;
+                    }
                 }
                 else
                 {
                     continue;
                 }
+
             }
         }
     }
